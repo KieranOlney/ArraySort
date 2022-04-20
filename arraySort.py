8e55681278875f8a30744cc557b60bb36493e0c8
KieranOlney <kieran.olney@outlook.com>
2022-04-20 16:47:30 +0100

    initial commit of main file for version control

---
+++ arraySort.py
@@ -0,0 +1,42 @@
+import random
+
+def main():
+    testArrayOne = generateArray(100)
+    testArrayTwo = generateArray(20)
+    testArrayThree = []
+    testArrayFour = None
+    print(testArrayOne)
+    print(sortArray(testArrayOne),"\n\n")
+    print(testArrayTwo)
+    print(sortArray(testArrayTwo),"\n\n")
+    print(sortArray(testArrayThree),"\n\n")
+    print(sortArray(testArrayFour),"\n\n")
+    return
+
+def sortArray(arrayToSort):
+    newArray = []
+    if arrayToSort == None:
+        print("Array Does Not Exist To Sort")
+        return newArray
+    elif len(arrayToSort) == 0:
+        print("Array Is Empty")
+        return newArray
+    else:
+        while arrayToSort != []:
+            arrayMinimum = arrayToSort[0]
+            for i in arrayToSort:
+                if i < arrayMinimum:
+                    arrayMinimum = i
+
+            newArray.append(arrayMinimum)
+            arrayToSort.remove(arrayMinimum)
+        print("Array Sorted")
+        return newArray
+
+def generateArray(length):
+    newArray = []
+    for i in range(length):
+        newArray.append(random.randint(0,10000))
+    return newArray
+
+main()
